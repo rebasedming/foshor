a1b3b7e06e86feb049707f21de6a9e93af3ca9a1
mingy98 <ming@fractal.co>
2021-02-27 21:26:30 -0500

    audio playing not working

--- src/components/voiceButton.tsx
+++ src/components/voiceButton.tsx
@@ -1,28 +1,43 @@
-import { NONAME } from "dns"
-import React from "react"
+import React from "react";
+import useSound from "use-sound";
 
-const VoiceButton = (props: {
-    title: string
-}) => {
-    const {title} = props 
+const Chaatreuse = require("./chaatreuse.mp3");
 
-    return(
-        <>
-        <button style = {{
-            border: "none",
-            padding: "10px 30px",
-            borderRadius: 5,
-            outline: "none",
-            background: "blue",
-            color: "white",
-            marginBottom: 20,
-            fontSize: 20,
-            cursor: "pointer"
-        }}>
-            {title}
-        </button>
-        </>
-    )
-}
+const VoiceButton = (props: { title: string }) => {
+  const { title } = props;
 
-export default VoiceButton
+  const playSound = () => {
+    let audio = new Audio()
+    audio.src = "./chaatreuse.mp3"
+    audio.load()
+    if (audio) {
+      audio
+        .play()
+        .then(() => console.log("playing"))
+        .catch((err: any) => console.log(err));
+    }
+  };
+
+  return (
+    <>
+      <button
+        style={{
+          border: "none",
+          padding: "10px 30px",
+          borderRadius: 5,
+          outline: "none",
+          background: "blue",
+          color: "white",
+          marginBottom: 20,
+          fontSize: 20,
+          cursor: "pointer",
+        }}
+        onClick={playSound}
+      >
+        {title}
+      </button>
+    </>
+  );
+};
+
+export default VoiceButton;
